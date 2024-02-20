d270c874f224101c8c2576c2155a4e531837836a
Viktoria <vincenzomadrid@gmail.com>
2024-02-20 15:56:41 +0100

    Update custom-cursor.js

--- js/custom-cursor.js
+++ js/custom-cursor.js
@@ -14,17 +14,24 @@ document.addEventListener("mousemove", function (e) {
     }, { duration: 600, fill: "forwards" });
 });
 
-document.querySelectorAll(".video-container, .details__vid").forEach(element => {
+document.querySelectorAll(".video-container, .details--open").forEach(element => {
     element.addEventListener("mouseover", function () {
         cursorDot.style.width = "64px";
         cursorDot.style.height = "64px";
         cursorDot.style.color = "white";
-        cursorDot.style.backgroundColor = "hsla(340, 74%, 40%, 1);";
+        cursorDot.style.zIndex = "9999";
+        cursorOutline.style.zIndex = "9998"; 
+        cursorOutline.style.border = "2px solid transparent";
+        cursorDot.style.backgroundColor = "hsla(340, 74%, 40%, 1)";
         if (element.classList.contains("video-container")) {
             cursorDot.innerHTML = "Open";
-        } else if (element.classList.contains("details__vid")) {
+        } else if (element.classList.contains("details--open")) {
             cursorDot.innerHTML = "Close";
         }
+    });
+
+    cursorOutline.addEventListener("mouseover", function () {
+        cursorOutline.style.border = "2px solid hsla(340, 74%, 40%, 1)";
     });
 
     element.addEventListener("mouseout", function () {
@@ -32,27 +39,8 @@ document.querySelectorAll(".video-container, .details__vid").forEach(element => 
         cursorDot.style.height = "14px";
         cursorDot.innerHTML = "";
         setTimeout(() => {
-            cursorDot.style.backgroundColor = "hsla(322, 67%, 49%, 1)";
+            cursorDot.style.backgroundColor = "hsla(340, 74%, 40%, 1)";
+            cursorOutline.style.border = "2px solid hsla(224, 41%, 40%, 1)";
         }, 100);
     });
 });
-
-
-// const videoContainers = document.querySelectorAll('.video-container');
-// const cursorElements = document.querySelectorAll('.cursor-white-circle, .cursor-white-outline, .cursor-dot, .cursor-outline');
-
-// videoContainers.forEach(videoContainer => {
-//     videoContainer.addEventListener('mouseover', () => {
-//         cursorElements.forEach(element => {
-//             element.style.opacity = '0';
-//             element.style.pointerEvents = 'none';
-//         });
-//     });
-
-//     videoContainer.addEventListener('mouseout', () => {
-//         cursorElements.forEach(element => {
-//             element.style.opacity = '';
-//             element.style.pointerEvents = '';
-//         });
-//     });
-// });
